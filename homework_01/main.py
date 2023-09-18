e52bf0cc8f66edd485cd3e56dcbae635892659c2
Mikhail Murzaev <mikhail.murzaev@gmail.com>
2023-09-18 22:15:52 +0500

    Updated power_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [x ** 2 for x in args]
 
 
 # filter types
