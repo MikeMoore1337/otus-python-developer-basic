17a2b881cac554938cf8d1a1578d0c9fa4620144
Mikhail Murzaev <mikhail.murzaev@gmail.com>
2023-09-30 17:59:24 +0300

    Updated exceptions.py

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -6,16 +6,20 @@
 """
 
 
-class LowFuelError(Exception):
+class BaseVehicleException(Exception):
+    pass
+
+
+class LowFuelError(BaseVehicleException):
     def __init__(self, message="Not enough fuel to start"):
         super().__init__(message)
 
 
-class NotEnoughFuel(Exception):
+class NotEnoughFuel(BaseVehicleException):
     def __init__(self, message="Not enough fuel to cover the distance"):
         super().__init__(message)
 
 
-class CargoOverload(Exception):
+class CargoOverload(BaseVehicleException):
     def __init__(self, message="Cargo capacity exceeded"):
         super().__init__(message)
