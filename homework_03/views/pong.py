7694e858a4c7b3bdf66d220397cbd377242704b1
Mikhail Murzaev <mikhail.murzaev@gmail.com>
2023-10-23 16:34:29 +0300

    Updated homework_03

--- homework_03/views/pong.py
+++ homework_03/views/pong.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/ping", tags=["Ping"])
+router = APIRouter()
 
 
 @router.get("/ping/")
 def get_answer():
-    return {"data": "pong"}
+    return {"message": "pong"}
