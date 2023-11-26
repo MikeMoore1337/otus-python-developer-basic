d4931a17dd44fe78b3c4bfb1163dd0e930c461e0
Mikhail Murzaev <mikhail.murzaev@gmail.com>
2023-11-26 17:29:57 +0300

    Work with homework_04

--- homework_04/main.py
+++ homework_04/main.py
@@ -35,21 +35,25 @@ async def async_main():
         # Создание пользователей
         for user_data in users_data:
             user = User(
-                id=user_data["id"],
+                id=user_data.get("id"),
                 name=user_data["name"],
                 username=user_data["username"],
                 email=user_data["email"],
-                phone=user_data["phone"],
-                website=user_data["website"],
-                company_id=user_data["company"]["id"],
-                address_id=user_data["address"]["id"],
+                phone=user_data.get("phone"),
+                website=user_data.get("website"),
+                company_id=user_data["company"].get("id")
+                if user_data.get("company")
+                else None,
+                address_id=user_data["address"].get("id")
+                if user_data.get("address")
+                else None,
             )
             users.append(user)
 
         # Создание постов
         for post_data in posts_data:
             post = Post(
-                id=post_data["id"],
+                id=post_data.get("id"),
                 user_id=post_data["userId"],
                 title=post_data["title"],
                 body=post_data["body"],
