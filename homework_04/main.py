ed8ab15d6a80c605e469b4663f7ab6757e61c192
Mikhail Murzaev <mikhail.murzaev@gmail.com>
2023-11-26 18:52:18 +0300

    Work with homework_04

--- homework_04/main.py
+++ homework_04/main.py
@@ -16,7 +16,7 @@
 import asyncio
 
 from jsonplaceholder_requests import fetch_posts_data, fetch_users_data
-from models import AsyncSession, Base, Post, User, engine
+from models import Address, AsyncSession, Base, Geo, Post, User, engine
 
 
 async def async_main():
@@ -29,40 +29,44 @@ async def async_main():
     )
 
     async with AsyncSession(bind=engine) as session:
-        users = [
-            User(
+        for user_data in users_data:
+            user = User(
                 id=user_data["id"],
                 name=user_data["name"],
                 username=user_data["username"],
                 email=user_data["email"],
                 phone=user_data.get("phone"),
                 website=user_data.get("website"),
-                company_id=(
-                    user_data["company"]["id"]
-                    if "company" in user_data and "id" in user_data["company"]
-                    else None
-                ),
-                address_id=(
-                    user_data["address"]["id"]
-                    if "address" in user_data and "id" in user_data["address"]
-                    else None
-                ),
+                company_id=user_data["company"]["id"],
+                address_id=user_data["address"]["id"],
             )
-            for user_data in users_data
-        ]
+            session.add(user)
 
-        posts = [
-            Post(
+            address_data = user_data["address"]
+            geo_data = address_data["geo"]
+
+            geo = Geo(id=geo_data["id"], lat=geo_data["lat"], lng=geo_data["lng"])
+            session.add(geo)
+
+            address = Address(
+                id=address_data["id"],
+                street=address_data["street"],
+                suite=address_data["suite"],
+                city=address_data["city"],
+                zipcode=address_data["zipcode"],
+                geo_id=geo_data["id"],
+            )
+            session.add(address)
+
+        for post_data in posts_data:
+            post = Post(
                 id=post_data["id"],
                 user_id=post_data["userId"],
                 title=post_data["title"],
                 body=post_data["body"],
             )
-            for post_data in posts_data
-        ]
+            session.add(post)
 
-        session.add_all(users)
-        session.add_all(posts)
         await session.commit()
 
 
