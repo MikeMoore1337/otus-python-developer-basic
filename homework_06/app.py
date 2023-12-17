f833e42b816fc402fbd511712c05ed0ab156fb6a
m.murzaev <m.murzaev@timeweb.ru>
2023-12-17 15:16:38 +0300

    homework_06 is ready to review

--- homework_06/app.py
+++ homework_06/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, redirect, render_template, request
-
 from models import Record, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@db/mydatabase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://myuser:mypassword@db/mydatabase"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "your_secret_key"
 
 db.init_app(app)
 
@@ -18,32 +17,30 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/records/', methods=['GET', 'POST'], endpoint='records')
+@app.route("/records/", methods=["GET", "POST"], endpoint="records")
 def records():
-    if request.method == 'POST':
-        content = request.form.get('content')
+    if request.method == "POST":
+        content = request.form.get("content")
         new_record = Record(content=content)
         db.session.add(new_record)
         db.session.commit()
-        return redirect('/records/')
+        return redirect("/records/")
     else:
         records = Record.query.all()
-        return render_template('records.html', records=records)
+        return render_template("records.html", records=records)
 
 
-@app.route('/view_records', methods=['GET', 'POST'], endpoint='view_records')
+@app.route("/view_records", methods=["GET", "POST"], endpoint="view_records")
 def view_records():
-    if request.method == 'POST':
-        # Получите идентификатор записи, которую нужно удалить
-        record_id = request.form.get('record_id')
-        # Найдите запись в базе данных по идентификатору и удалите ее
+    if request.method == "POST":
+        record_id = request.form.get("record_id")
         record = Record.query.get(record_id)
         if record:
             db.session.delete(record)
             db.session.commit()
 
     records = Record.query.all()
-    return render_template('view_records.html', records=records)
+    return render_template("view_records.html", records=records)
 
 
 @app.route("/about/", endpoint="about")
@@ -52,4 +49,4 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
