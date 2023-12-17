a6608fb14a1e09f73d53c90b69c4c979a48315a8
m.murzaev <m.murzaev@timeweb.ru>
2023-12-17 18:06:53 +0300

    homework_06 is ready to review

--- homework_06/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class Record(db.Model):
-    __tablename__ = "record"
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(255), nullable=False)
